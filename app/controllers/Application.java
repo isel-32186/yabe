92e3bd36ab6d331cf40b8fb081f6f23e0f46cea5
Luis Loureiro <32186@alunos.isel.pt>
2012-04-25 18:13:51 +0100

    Viewing and posting comments.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.data.validation.Required;
 import play.mvc.*;
 
 import java.util.*;
@@ -15,10 +16,25 @@ public class Application extends Controller {
         	"order by postedAt desc").from(1).fetch(10);
         render(frontPost, olderPosts);
     }
+    
+    public static void show(long id) {
+    	Post post = Post.findById(id);
+    	render(post);
+    }
 
     @Before
     static void addDefaults() {
         renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
         renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+    }
+    
+    public static void postComment(long postId, @Required String author, @Required String content) {
+    	Post post = Post.findById(postId);
+    	if(validation.hasErrors()) {
+    		render("Application/show.html", post);
+    	}
+    	post.addComment(author, content);
+        flash.success("Thanks for posting %s", author);
+    	show(postId);
     }
 }
