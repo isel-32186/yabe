ae91e6077eb31900f5a6207e4eed3482caa83602
Luis Loureiro <32186@alunos.isel.pt>
2012-04-25 19:35:45 +0100

    Adding tagging support.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,10 @@
 package controllers;
 
 import play.*;
+import play.cache.Cache;
 import play.data.validation.Required;
+import play.libs.Codec;
+import play.libs.Images;
 import play.mvc.*;
 
 import java.util.*;
@@ -19,7 +22,8 @@ public class Application extends Controller {
     
     public static void show(long id) {
     	Post post = Post.findById(id);
-    	render(post);
+    	String randomID = Codec.UUID();
+    	render(post, randomID);
     }
 
     @Before
@@ -28,13 +32,35 @@ public class Application extends Controller {
         renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
     }
     
-    public static void postComment(long postId, @Required String author, @Required String content) {
+    public static void postComment(long postId, 
+    		@Required(message="Author is required") String author, 
+    		@Required(message="A message is required") String content,
+    		@Required(message="Please type the code") String code,
+    		String randomID)
+    {
     	Post post = Post.findById(postId);
+    	validation.equals(code, Cache.get(randomID)).message("Invalid code. Please type it again.");
+    	
     	if(validation.hasErrors()) {
     		render("Application/show.html", post);
     	}
     	post.addComment(author, content);
         flash.success("Thanks for posting %s", author);
+        
+        Cache.delete(randomID);
+        
     	show(postId);
+    }
+    
+    public static void captcha(String id) {
+    	Images.Captcha captcha = Images.captcha();
+    	String code = captcha.getText("#E4EAFD");
+    	Cache.set(id, code, "10mn");
+    	renderBinary(captcha);
+    }
+    
+    public static void listTagged(String tag) {
+        List<Post> posts = Post.findTaggedWith(tag);
+        render(tag, posts);
     }
 }
--- app/models/Post.java
+++ app/models/Post.java
@@ -3,10 +3,13 @@ package models;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
@@ -27,12 +30,16 @@ public class Post extends Model {
     @OneToMany(mappedBy="post", cascade=CascadeType.ALL)
     public List<Comment> comments;
     
+    @ManyToMany(cascade=CascadeType.PERSIST)
+    public Set<Tag> tags;
+    
     public Post(User author, String title, String content) {
         this.author = author;
         this.title = title;
         this.content = content;
         this.postedAt = new Date();
         this.comments = new LinkedList<Comment>();
+        this.tags = new TreeSet<Tag>();
     }
     
     public Post addComment(String author, String content) {
@@ -40,6 +47,23 @@ public class Post extends Model {
     	this.comments.add(newComment);
     	this.save();
     	return this;
+    }
+    
+    public Post tagItWith(String name) {
+    	tags.add(Tag.findOrCreateByName(name));
+    	return this;
+    }
+    
+//    public static List<Post> findTaggedWith(String tag) {
+//    	return Post.find(
+//    			"select distinct p from Post p join p.tags as t where t.name = ?", tag
+//    			).fetch();
+//    }
+    public static List<Post> findTaggedWith(String... tags) {
+        return Post.find(
+                "select distinct p from Post p join p.tags as t where t.name in (:tags) " +
+                "group by p.id, p.author, p.title, p.content, p.postedAt having count(t.id) = :size"
+        ).bind("tags", tags).bind("size", tags.length).fetch();
     }
     
     public Post previous() {
